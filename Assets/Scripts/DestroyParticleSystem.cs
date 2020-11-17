4dcbf029635a95b89213f8781e28b02166d5ab39
cjabs1997 <cjabs97@gmail.com>
2020-11-17 13:35:13 -0800

    Explosions now clean themselves up and added more particles to the notes
    -Notes emit a particle trail as they move down

---
+++ Assets/Scripts/DestroyParticleSystem.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// Destroys a gameobject with a particle system after it has finished its emission.
+/// </summary>
+public class DestroyParticleSystem : MonoBehaviour
+{
+    [Tooltip("Use to override how long a particle system exists for. Leave below zero to be ignored.")]
+    public float destroyTimeOverride = -1;
+    [Tooltip("Additional delay on the destroy time. Optional, use as needed if things are getting cut off.")]
+    public float destroyDelay = 0f;
+
+    private ParticleSystem m_ParticleSystem;
+    private float destroyTime;
+
+
+    private void Awake()
+    {
+        m_ParticleSystem = this.GetComponent<ParticleSystem>();
+    }
+
+    private void Start()
+    {
+        SetDestroyTime();
+
+        Destroy(this.gameObject, destroyTime);
+    }
+
+    private void SetDestroyTime()
+    {
+        if (destroyTimeOverride > 0)
+            destroyTime = destroyTimeOverride;
+        else
+        {
+            destroyTime = m_ParticleSystem.main.duration + m_ParticleSystem.main.startLifetimeMultiplier + destroyDelay;
+        }
+    }
+}
