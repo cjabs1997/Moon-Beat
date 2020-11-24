98c05eca6e635daebf5a6cef5f96e410cb8a39bd
cjabs1997 <cjabs97@gmail.com>
2020-11-23 16:26:45 -0800

    Shifted note movement to an animation instead
    -Also added button to composer editor to start the song
    -Still need some better cleanup for when the animation finishes

---
+++ Assets/Scripts/Editor/ComposerEditor.cs
@@ -0,0 +1,18 @@
+﻿using UnityEditor;
+using UnityEngine;
+
+[CustomEditor(typeof(Composer))]
+public class ComposerEditor : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        base.OnInspectorGUI();
+
+        Composer composer = (Composer)target;
+
+        if(GUILayout.Button("Start Song"))
+        {
+            composer.startSong();
+        }
+    }
+}
--- Assets/Scripts/NoteController.cs
+++ Assets/Scripts/NoteController.cs
@@ -19,7 +19,6 @@ public class NoteController : MonoBehaviour
 
     private void Awake()
     {
-        this.GetComponent<Rigidbody2D>().velocity = Vector2.down * 2.5f;
         m_CinemachineImpulseSource = this.GetComponent<CinemachineImpulseSource>();
     }
 
@@ -37,7 +36,7 @@ public class NoteController : MonoBehaviour
     {
         GameObject.Instantiate(explosionEffect, this.transform.position, Quaternion.identity);
         m_CinemachineImpulseSource.GenerateImpulse();
-        //Debug.Log("DISTANCE: " + (this.gameObject.transform.position.y - buttonY));
-        Destroy(this.gameObject);
+        Debug.Log("DISTANCE: " + (this.gameObject.transform.position.y - buttonY));
+        Destroy(this.transform.parent.gameObject);
     }
 }
