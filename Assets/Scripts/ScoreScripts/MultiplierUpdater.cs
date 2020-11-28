fedb04f593be4ec6c331e30106704b0ca4e05350
cjabs1997 <cjabs97@gmail.com>
2020-11-28 11:53:17 -0800

    Added score multiplier text and tweaked particle glow + screen shake
    -Lowered particle glow
    -Lowered shake applitude
    -Increased shake frequency
    -Lowered shake sustain

---
+++ Assets/Scripts/ScoreScripts/MultiplierUpdater.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MultiplierUpdater : MonoBehaviour
+{
+    [Tooltip("ScriptableObject that holds the current score.")]
+    public FloatVariable multiplierVariable;
+    [Tooltip("The text that comes before the multiplier, '____ <multiplier> ...'.")]
+    public string prefaceText;
+    [Tooltip("The text that comes after the multiplier, '... <multiplier> ____'.")]
+    public string suffixText;
+
+    TMPro.TextMeshProUGUI m_TextMeshProUGUI;
+
+    private void Awake()
+    {
+        m_TextMeshProUGUI = this.GetComponent<TMPro.TextMeshProUGUI>();
+    }
+
+
+    public void UpdateMultiplier()
+    {
+        m_TextMeshProUGUI.text = prefaceText + " " + multiplierVariable.Value.ToString("0.0") + suffixText;
+    }
+}
