028de403c25f3ee92ed82aeb8ed1239fe80b07b4
cjabs1997 <cjabs97@gmail.com>
2020-11-29 19:50:59 -0800

    Added pause screen

---
+++ Assets/Scripts/UIScripts/PauseScreen.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PauseScreen : MonoBehaviour
+{
+    public KeyCode pauseKey;
+    public AudioSource audioSource;
+
+    private GameObject pauseMenu;
+    private float defaultTimeScale;
+
+    private void Awake()
+    {
+        pauseMenu = this.transform.GetChild(0).gameObject;
+        defaultTimeScale = Time.timeScale;
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(pauseKey))
+        {
+            if(pauseMenu.activeSelf == true)
+            {
+                Resume();
+            }
+            else
+            {
+                Pause();
+            }
+        }
+    }
+
+    public void Resume()
+    {
+        Time.timeScale = defaultTimeScale;
+        audioSource.UnPause();
+        pauseMenu.SetActive(false);
+    }
+
+    public void Pause()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+        audioSource.Pause();
+    }
+}
